d2b63352315c13c6f1b2841354e59a8b6fd15dcc
Tilak <125390274+YourTilak@users.noreply.github.com>
2023-06-16 08:15:20 +0530

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -11,9 +11,6 @@ const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
 
-// Custom
-bot.command("yoi", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
-
 // Handle the /effect command to apply text effects using an inline keyboard
 type Effect = { code: TextEffectVariant; label: string };
 const allEffects: Effect[] = [
@@ -163,7 +160,6 @@ const aboutUrlKeyboard = new InlineKeyboard().url(
 // Suggest commands in the menu
 bot.api.setMyCommands([
   { command: "yo", description: "Be greeted by the bot" },
-  { command: "yoi", description: "Just dododoododododod" },
   {
     command: "effect",
     description: "Apply text effects on the text. (usage: /effect [text])",
